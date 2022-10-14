6a00aafad58417d06b70d1aa6f9db4a4962cf445
Omena0 <Omena0MC@gmail.com>
2022-10-14 17:46:39 +0300

    V2!

--- code/app.py
+++ code/app.py
@@ -1,4 +1,45 @@
+
 #Generated with flask-server-generator!
 #View it on github: https://github.com/Omena0/flask-server-generator
-from flask import Flask
+
+from flask import *
+import lib
 app = Flask(__name__)
+app.secret_key=b"ce8a093b2b15e8de1c2620aaebfe24f4a7c12437222ccc93646e5b0cd88e9921"
+
+@app.route("/")
+def __home__():
+    if True and "username" not in session: return redirect(url_for("login"))
+    file = open(".html")
+    content = file.read()
+    file.close()
+    return content
+    
+@app.errorhandler(404)
+def __404__(a):
+    try: lib.log('!',session["username"]+' Recieved 404 page!')
+    except: lib.log('!','UNKNOWN USER Recieved 404 page!')
+    file = open("404.html")
+    content = file.read()
+    file.close()
+    return content
+    
+@app.route("/main")
+def main():
+    if True and "username" not in session: return redirect(url_for("login"))
+    file = open("main.html")
+    content = file.read()
+    file.close()
+    return content
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        lib.log('LOGIN',session["username"]+' Logged in.')
+        return redirect(url_for("__home__"))
+    file = open("__login__.html")
+    content = file.read()
+    file.close()
+    return content
+    
--- code/generator.py
+++ code/generator.py
@@ -2,14 +2,44 @@ print('Initializing... [OS, Lib]')
 import os
 import lib
 
+####### CONFIG #######
+
+# Root site function name in app.py
+__root__ = '__home__'
+
+# Name of html file set as root, recommended to set as '' or '__home__' 
+rootname = ''#.html
+
+# Where to go after login
+logonredirect = __root__
+
+# Enable auth with %auth=True%
+auth_enabled = True
+
+# Force auth on every page and redirect to login page
+force_auth = True
+
+
+
+#### END CONFIG ####
+
 lib.log('*','Initialized!')
 
 app = open('app.py','wb')
 
 lib.log('*','Initializing app...')
 
-header = '#Generated with flask-server-generator!\n#View it on github: https://github.com/Omena0/flask-server-generator\n'
-code = 'from flask import Flask\napp = Flask(__name__)\n'
+header = '''
+#Generated with flask-server-generator!
+#View it on github: https://github.com/Omena0/flask-server-generator
+'''
+code = '''
+from flask import *
+import lib
+app = Flask(__name__)
+app.secret_key=b"ce8a093b2b15e8de1c2620aaebfe24f4a7c12437222ccc93646e5b0cd88e9921"
+'''
+
 app.write(header.encode())
 app.write(code.encode())
 app.close()
@@ -45,9 +75,63 @@ app = open('app.py','ab')
 index = 0
 lib.log('*','App file openned!\n')
 for i in names:
-    if i == '': name = '__home__'
-    else: name = i 
-    a = f'@app.route("/{i}")\ndef {name}():\n    file = open("{i}.html")\n    content = file.read()\n    file.close()\n    return content\n'
+    if i == rootname:
+        a = f'''
+@app.route("/{i}")
+def __home__():
+    if {force_auth} and "username" not in session: return redirect(url_for("login"))
+    file = open("{i}.html")
+    content = file.read()
+    file.close()
+    return content
+    '''
+            
+    elif i == '404': a = f'''
+@app.errorhandler(404)
+def __404__(a):
+    try: lib.log('!',session["username"]+' Recieved 404 page!')
+    except: lib.log('!','UNKNOWN USER Recieved 404 page!')
+    file = open("{i}.html")
+    content = file.read()
+    file.close()
+    return content
+    '''
+    elif i == '__login__':
+        a = f'''
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        lib.log('LOGIN',session["username"]+' Logged in.')
+        return redirect(url_for("{logonredirect}"))
+    file = open("__login__.html")
+    content = file.read()
+    file.close()
+    return content
+    '''
+    elif '%auth=True%' in i:
+        if not auth_enabled: continue
+        i = i.replace('%auth=True%','')
+        a = f'''
+@app.route("/{i}")
+def {i}():
+    if {force_auth} and "username" not in session: return redirect(url_for("login"))
+    if 'username' in session:
+        file = open("{i}.html")
+        content = file.read()
+        file.close()
+        return content
+    return "You are not logged in!"
+'''
+    else: a = f'''
+@app.route("/{i}")
+def {i}():
+    if {force_auth} and "username" not in session: return redirect(url_for("login"))
+    file = open("{i}.html")
+    content = file.read()
+    file.close()
+    return content
+'''
     lib.log(i,f'Writing function: {a}')
     lib.log('*',f'Return from file.write():  {app.write(a.encode())}')
     index = index + 1
